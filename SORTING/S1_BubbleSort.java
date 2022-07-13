79bc05c17e16a17bbd70205dcaaa05305f4c1428
Pranav <powar.pranav29@gmail.com>
2022-07-13 08:08:38 +0530

    insertionsort

--- SORTING/S1_BubbleSort.java
+++ SORTING/S1_BubbleSort.java
@@ -2,7 +2,7 @@ package SORTING;
 
 public class S1_BubbleSort{
     public static void main(String[] args){
-        int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
         for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
             for(int i =0; i < lastUnsortedIndex; i++){
