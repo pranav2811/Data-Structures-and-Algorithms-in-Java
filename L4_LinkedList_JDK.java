2bf2fa06120ce193099ecb9509673f602e2ab9e2
Pranav <powar.pranav29@gmail.com>
2022-08-02 23:01:56 +0530

    linked list using jdk

--- L4_LinkedList_JDK.java
+++ L4_LinkedList_JDK.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 class Employee {
 
     private String firstName;
@@ -70,5 +73,56 @@ public class L4_LinkedList_JDK {
             Employee johnDoe = new Employee("John", "Doe", 4567);
             Employee marySmith = new Employee("Mary", "Smith", 22);
             Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+            Employee billEnd = new Employee("Bill", "End", 78);
+
+            LinkedList<Employee> list = new LinkedList<>();
+            list.addFirst(janeJones);
+            list.add(johnDoe);
+            list.addFirst(marySmith);
+            list.addFirst(mikeWilson);
+
+
+            Iterator<Employee> iter = list.iterator();
+            System.out.println("HEAD ->");
+
+            while (iter.hasNext()) {
+                System.out.println(iter.next());
+                System.out.println("<=>");
+            }
+            System.out.println("null");
+
+            list.add(billEnd);//add method adds an item to the end of the list that is to the tail, addLast method can also be used
+            iter = list.iterator();
+            System.out.println("HEAD ->");
+
+            while (iter.hasNext()) {
+                System.out.println(iter.next());
+                System.out.println("<=>");
+            }
+            System.out.println("null");
+
+            list.removeFirst();//removes the first item from the list
+            
+            iter = list.iterator();
+            System.out.println("HEAD ->");
+
+            while (iter.hasNext()) {
+                System.out.println(iter.next());
+                System.out.println("<=>");
+            }
+            System.out.println("null");
+
+            list.removeLast();//removes the last item from the list
+            iter = list.iterator();
+            System.out.println("HEAD ->");
+
+            while (iter.hasNext()) {
+                System.out.println(iter.next());
+                System.out.println("<=>");
+            }
+            System.out.println("null");
+            // for (Employee employee : list) {
+            //     System.out.println(employee);
+            // }
     }
 }
