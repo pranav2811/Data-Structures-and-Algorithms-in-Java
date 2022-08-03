b658acb9f42cbc53b73ce91f6023abe90af1d06a
Pranav <powar.pranav29@gmail.com>
2022-08-03 09:31:59 +0530

    stacks using linkedList

---
+++ L5_Stack_LinkedList.java
@@ -0,0 +1,129 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+class Employee {
+
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        if (!firstName.equals(employee.firstName)) return false;
+        return lastName.equals(employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + id;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+}
+//java docs suggest that instead of using the array class we should use a class that uses the deque interface
+class LinkedStack{
+
+    private LinkedList<Employee> stack;
+
+    public LinkedStack(){
+        stack = new LinkedList<Employee>();//For stack you dont need a doublyLinkedList as you are always workign with thte item at the front of the array but memory is not an issue so we can go ahead
+    }
+
+    public void push(Employee employeee){
+        stack.push(employeee);
+    }
+
+    public Employee pop(){
+        return stack.pop();
+    }
+    
+    public Employee peek(){
+        return stack.peek();
+    }
+
+    public boolean isEmpty(){
+        return stack.isEmpty();
+    }
+
+    public void printStack(){
+        ListIterator<Employee> iterator = stack.listIterator();
+        while(iterator.hasNext()){//this iteratorlist traverses from head to tail
+            System.out.println(iterator.next());
+        }
+    }
+}
+
+public class L5_Stack_LinkedList {
+    public static void main(String[] args) {
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee marySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+        Employee billEnd = new Employee("Bill", "End", 78);
+        
+        LinkedStack stack = new LinkedStack();
+
+        stack.push(janeJones);
+        stack.push(johnDoe);
+        stack.push(marySmith);
+        stack.push(mikeWilson);
+        stack.push(billEnd);
+
+        stack.printStack();
+        
+        System.out.println(stack.peek());
+        stack.printStack();
+
+        System.out.println("Popped: " + stack.pop());
+        System.out.println(stack.peek());
+
+        
+    }
+}
