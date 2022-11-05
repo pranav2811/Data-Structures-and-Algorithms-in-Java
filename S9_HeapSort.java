5674369e4e675a4c1ffefc708c5f59e166687cc8
Pranav <powar.pranav29@gmail.com>
2022-11-06 02:52:12 +0530

    Heap Sort

---
+++ S9_HeapSort.java
@@ -0,0 +1,165 @@
+
+/*
+ * Can only be used for sorting heaps
+ * we know the root has the largest value
+ * Swap root with last element in the array
+ * Heapify the tree, but exclude the last node
+ * After heapify, second largest element is at the root
+ * Rinse and repeat
+ * 
+ * Worst Case time complexity : O(n log n) as we are swapping n elements and at each iteration we fix heap whcih is O(log n)
+ * After sorting the array it is no longer a heap and so we would not want to sort a heap that we want to keep reducing
+ */
+
+class Heap {
+
+    private int[] heap;
+    private int size;
+
+    public Heap(int capacity) {
+        heap = new int[capacity];
+    }
+
+    public void insert(int value) {//normally it is constant time complexity but we need to fix heap so it is o(log(n))
+        if (isFull()) {
+            throw new IndexOutOfBoundsException("Heap is full");
+        }
+
+        heap[size] = value;
+
+        fixHeapAbove(size);
+        size++;
+    }
+
+    //for peek we just return heap[0] if the heap is not empty and if empty we throw an esxception
+
+    public int peek(){
+        if(isEmpty()){
+            throw new IndexOutOfBoundsException();
+        }
+        return heap[0];
+    }
+
+    public int delete(int index) {//first we need to find the item we need to remove, in worst case thatll be linear cuz linear search, then we need to fix heap so it is o(log(n))
+        //but with heaps we just want to remove the root and then fix the heap so it is o(log(n))
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+
+        int parent = getParent(index);
+        int deletedValue = heap[index];
+
+        heap[index] = heap[size - 1];
+
+        if (index == 0 || heap[index] < heap[parent]) {
+            fixHeapBelow(index, size - 1);
+        }
+        else {
+            fixHeapAbove(index);
+        }
+
+        size--;
+
+        return deletedValue;
+
+    }
+
+    private void fixHeapAbove(int index) {
+        int newValue = heap[index];
+        while (index > 0 && newValue > heap[getParent(index)]) {
+            heap[index] = heap[getParent(index)];
+            index = getParent(index);
+        }
+
+        heap[index] = newValue;
+    }
+
+    public void sort(){
+        int lastHeapIndex = size - 1;
+        for(int i = 0; i < lastHeapIndex; i ++){
+            int temp = heap[0];//swap the root with the last item
+            heap[0] = heap[lastHeapIndex - i];
+            heap[lastHeapIndex - i] = temp;
+
+            fixHeapBelow(0, lastHeapIndex - i - 1);//lastHeapIndex - i - 1 as we want to exclude where we have put the root
+        }
+    }
+
+    private void fixHeapBelow(int index, int lastHeapIndex) {
+        int childToSwap;
+
+        while (index <= lastHeapIndex) {
+            int leftChild = getChild(index, true);
+            int rightChild = getChild(index, false);
+            if (leftChild <= lastHeapIndex) {
+                if (rightChild > lastHeapIndex) {
+                    childToSwap = leftChild;
+                }
+                else {
+                    childToSwap = (heap[leftChild] > heap[rightChild] ? leftChild : rightChild);
+                }
+
+                if (heap[index] < heap[childToSwap]) {
+                    int tmp = heap[index];
+                    heap[index] = heap[childToSwap];
+                    heap[childToSwap] = tmp;
+                }
+                else {
+                    break;
+                }
+
+                index = childToSwap;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    public void printHeap() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(heap[i]);
+            System.out.print(", ");
+        }
+        System.out.println();
+    }
+
+    public boolean isFull() {
+        return size == heap.length;
+    }
+
+    public int getParent(int index) {
+        return (index - 1) / 2;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getChild(int index, boolean left) {
+        return 2 * index + (left ? 1 : 2);
+    }
+
+}
+
+public class S9_HeapSort {
+    public static void main(String[] args) {
+
+        Heap heap = new Heap(10);
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.printHeap();//before sort
+        
+        heap.sort();
+
+        heap.printHeap();//after sort
+        
+    }
+}
