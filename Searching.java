9ac938f9582a9851eabee5a32a7f28fc9f8f9612
Pranav <powar.pranav29@gmail.com>
2022-11-24 03:43:22 +0530

    searching

--- Searching.java
+++ Searching.java
@@ -13,29 +13,35 @@ public class Searching {
     }
 
     // Binary Search
-    public static int binarySearch(int[] arr, int value){
+    public static int binarySearch(int[] arr, int value) {
         int start = 0;
         int end = arr.length - 1;
 
-        int middle = (start + end)/2;
+        int middle = (start + end) / 2;
 
-        while(arr[middle] != value && start <= end){
-            if(value < arr[middle]){
+        while (arr[middle] != value && start <= end) {
+            if (value < arr[middle]) {
                 end = middle - 1;
-        }
-    
-        else{
-            start = middle + 1;
-        }
-        middle = (start + end)/2;
+            }
 
+            else {
+                start = middle + 1;
+            }
+            middle = (start + end) / 2;
+
+            if (arr[middle] == value) {
+                System.out.println("The element found at the index " + middle);
+                return middle;
+            } else {
+                System.out.println("The element is not present in the array");
+                return -1;
+            }
+        }
 
     }
-    return middle;
-}
 
     public static void main(String[] args) {
-        int[] arr = {8, 15, 23, 27, 37};
+        int[] arr = { 8, 15, 23, 27, 37 };
         System.out.println(binarySearch(arr, 23));
     }
 }
