bca7c4f23fe077731cc6dd7f438100e25fd1ffce
Pranav <powar.pranav29@gmail.com>
2022-11-23 21:20:47 +0530

    matrix operations

---
+++ Arrays/matrixRotation.java
@@ -0,0 +1,26 @@
+package Arrays;
+//first we transpose the matrix and then reverse every row of the matrix
+//since we wont be creating any new matrix space complexity will be o(n1) and time complexity will be o(n^2)
+public class matrixRotation {
+    public void rotate(int[][] matrix) {
+        //first we transpose the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //now we reverse every row of the matrix using two pointer method
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length/2; j++){
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
+}
---
+++ Arrays/matrixTransposal.java
@@ -0,0 +1,22 @@
+package Arrays;
+public class matrixTransposal {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        //first we transpose the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //now we print the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
