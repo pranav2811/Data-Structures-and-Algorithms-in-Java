805a482c11339b7b225b4c65abed64c14088e632
Pranav <powar.pranav29@gmail.com>
2022-12-10 12:44:23 +0530

    directchaining insert

---
+++ Hashing/Main.java
@@ -0,0 +1,15 @@
+package Hashing;
+
+public class Main {
+    public static void main(String[] args) {
+        DirectChaining directChaining = new DirectChaining(10);
+        directChaining.insertHashTable("The");
+        directChaining.insertHashTable("quick");
+        directChaining.insertHashTable("brown");
+        directChaining.insertHashTable("fox");
+        directChaining.insertHashTable("over");
+        directChaining.insertHashTable("lazy");
+        directChaining.insertHashTable("dog");
+        directChaining.displayHashTable();
+    }
+}
