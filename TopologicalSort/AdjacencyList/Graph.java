04476b7b2494470090b9c6fc1880d479f2feb146
Pranav Powar <powar.pranav29@gmail.com>
2023-01-17 23:38:14 +0530

    topologicalSort adjacency list

---
+++ TopologicalSort/AdjacencyList/Graph.java
@@ -0,0 +1,73 @@
+package TopologicalSort.AdjacencyList;
+
+import java.util.*;
+
+public class Graph {
+
+    ArrayList<GraphNode> nodeList = new ArrayList<GraphNode>();
+
+    public Graph(ArrayList<GraphNode> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    public void addUndirectedEdge(int i, int j) {
+        GraphNode first = nodeList.get(i);
+        GraphNode second = nodeList.get(j);
+        first.neighbors.add(second);// add second ndoe as the nieghbour of the first node
+        second.neighbors.add(first);// add first node as the neighbour of the second node
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name + " : ");
+            for (int j = 0; j < nodeList.get(i).neighbors.size(); j++) {
+                if (j == nodeList.get(i).neighbors.size() - 1) {
+                    s.append(nodeList.get(i).neighbors.get(j).name);
+                } else {
+                    s.append(nodeList.get(i).neighbors.get(j).name + " -> ");
+                }
+            }
+            s.append("\n");
+        }
+        return s.toString();
+
+    }
+
+    // Topological Sort
+    public void addDirectedEdge(int i, int j) {
+        GraphNode first = nodeList.get(i);
+        GraphNode second = nodeList.get(j);
+        first.neighbors.add(second);
+
+    }
+
+    // helper method for topological sort
+
+    void topologicalVisit(GraphNode node, Stack<GraphNode> stack) {
+        // if not visited we will call method recursively and if visited then we will
+        // push it to the stack
+        for (GraphNode neighbor : node.neighbors) {
+            if (!neighbor.isVisited) {
+                topologicalVisit(neighbor, stack);
+            }
+        }
+        node.isVisited = true;
+        stack.push(node);
+
+    }
+
+    // the main topological sort method
+    void topologicalSort() {
+        Stack<GraphNode> stack = new Stack<>();
+        for (GraphNode node : nodeList) {
+            if (!node.isVisited) {
+                topologicalVisit(node, stack);
+            }
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop().name + " ");
+        }
+
+    }
+}
