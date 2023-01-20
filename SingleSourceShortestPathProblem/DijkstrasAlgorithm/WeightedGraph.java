1b0029f9add8a39420429b133e4241b1860665de
Pranav Powar <powar.pranav29@gmail.com>
2023-01-21 03:04:15 +0530

    dijkstras algo

---
+++ SingleSourceShortestPathProblem/DijkstrasAlgorithm/WeightedGraph.java
@@ -0,0 +1,57 @@
+package SingleSourceShortestPathProblem.DijkstrasAlgorithm;
+
+import java.util.*;
+
+public class WeightedGraph {
+    ArrayList<WeightedNode> nodeList = new ArrayList<WeightedNode>();
+
+    public WeightedGraph(ArrayList<WeightedNode> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    // implementing the dijkstras algo
+    void dijksta(WeightedNode node) {
+        // creating a priority queue and setting the distance of the first node to 0
+        PriorityQueue<WeightedNode> queue = new PriorityQueue<>();
+        node.distance = 0;
+        queue.addAll(nodeList);
+        // if queue is not empty we dequeu and check neighbors and update parents
+        while (!queue.isEmpty()) {
+            WeightedNode currentNode = queue.remove();
+            // check if neighbor is not visited and if known distance is greater we update
+            // distance
+            for (WeightedNode neighbor : currentNode.neighbors) {
+                if (queue.contains(neighbor)) {
+                    if (neighbor.distance > currentNode.distance + currentNode.weightMap.get(neighbor)) {
+                        neighbor.distance = currentNode.distance + currentNode.weightMap.get(neighbor);
+                        neighbor.parent = currentNode;
+                        queue.remove(neighbor);
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        for (WeightedNode nodeToCheck : nodeList) {
+            System.out.print("Node " + nodeToCheck + " distance: " + nodeToCheck.distance + " Path: ");
+            pathPrint(nodeToCheck);
+            System.out.println();
+
+        }
+    }
+
+    public static void pathPrint(WeightedNode node) {
+        if (node.parent != null) {
+            pathPrint(node.parent);
+        }
+        System.out.println(node.name + " ");
+
+    }
+
+    public void addWeightedEdge(int i, int j, int d){
+        WeightedNode first = nodeList.get(i);
+        WeightedNode second = nodeList.get(j);
+        first.neighbors.add(second);
+        first.weightMap.put(second, d);
+    }
+}
---
+++ SingleSourceShortestPathProblem/DijkstrasAlgorithm/WeightedNode.java
@@ -0,0 +1,32 @@
+package SingleSourceShortestPathProblem.DijkstrasAlgorithm;
+
+import java.util.*;
+
+public class WeightedNode  implements Comparable<WeightedNode> {
+    public String name;
+    public ArrayList<WeightedNode> neighbors = new ArrayList<WeightedNode>();
+    //HASH MAP TO STOP THE WEIGHT OF THE EDGE BETWEEN TWO NODES\
+    public HashMap<WeightedNode, Integer> weightMap = new HashMap<WeightedNode, Integer>();
+    public boolean isVisited = false;
+    public WeightedNode parent;
+    public int distance;
+    public int index;
+
+    public WeightedNode(String name, int index){
+        this.name = name;
+        distance = Integer.MAX_VALUE;
+        this.index = index;
+
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+
+    @Override
+    public int compareTo(WeightedNode otherNode){
+        return Integer.compare(this.distance, otherNode.distance);
+    }
+    
+}
