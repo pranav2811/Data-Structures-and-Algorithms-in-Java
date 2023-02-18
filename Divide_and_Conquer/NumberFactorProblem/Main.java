3911d031ead5a4dd0d3b85e573ac7d1023a66a6e
Pranav Powar <powar.pranav29@gmail.com>
2023-02-18 23:28:27 +0530

    number factor problem

---
+++ Divide_and_Conquer/NumberFactorProblem/Main.java
@@ -0,0 +1,9 @@
+package Divide_and_Conquer.NumberFactorProblem;
+
+public class Main {
+    public static void main(String[] args){
+        NumberFactor nf = new NumberFactor();
+        System.out.println(nf.waysToGetN(4));
+     }
+    
+}
