c6b7b5991c078912ea1c03ebdaa562920e71fb10
Pranav Powar <powar.pranav29@gmail.com>
2023-02-18 14:14:41 +0530

    FractionalKnapsack Problem

---
+++ FractionalKnapsack/KnapsackItem.java
@@ -0,0 +1,56 @@
+package FractionalKnapsack;
+
+public class KnapsackItem {
+    private int index;
+    private int value;
+    private int weight;
+    private double ratio;
+
+    
+
+    public KnapsackItem(int index, int value, int weight){
+        this.index = index;
+        this.value = value;
+        this.weight = weight;
+        ratio = value * 1.0 / weight;
+    }
+    
+    public int getIndex() {
+        return this.index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setRatio(double ratio) {
+        this.ratio = ratio;
+    }
+
+    public double getRatio(){
+        return this.ratio;
+    }
+
+    @Override
+    public String toString(){
+        return "Item index = " + index + ", value = " + value + ", weight = " + weight + ", ratio = " + ratio;
+    }
+    
+    
+}
---
+++ FractionalKnapsack/Main.java
@@ -0,0 +1,14 @@
+package FractionalKnapsack;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        ArrayList<KnapsackItem> items = new ArrayList<>();
+        int[] value = {100,120,60};
+        int[] weight = {20,30,10};
+        int capacity = 50;
+        for(int i = 0; i < value.length;i++){
+            items.add(new KnapsackItem(i+1,value[i],weight[i]));
+        }
+        FractionalKnapsack.knapsack(items,capacity);
+    }
+}
