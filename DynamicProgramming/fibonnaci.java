9baa33c255d7d06fbbba3e003239b79c75a6acd5
Pranav Powar <powar.pranav29@gmail.com>
2023-02-22 14:51:37 +0530

    top down with memoisation

---
+++ DynamicProgramming/fibonnaci.java
@@ -0,0 +1,31 @@
+package DynamicProgramming;
+import java.util.*;
+//using top down memoisation approach - i.e we start by breaking the problem from the stop and keep storing the solutions of the 
+//subproblems so that we dont have to calculate them again and again like divde and conquer
+
+
+public class fibonnaci {
+
+    public static void main(String[] args){
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        System.out.println(fibMemo(5, memo));
+    }
+    public static int fibMemo(int n, HashMap<Integer, Integer> memo){
+
+        if(n == 1){
+            return 0;
+        }
+        if(n == 2){
+            return 1;
+        }
+        if(!memo.containsKey(n)){
+            memo.put(n, (fibMemo(n -1, memo) + fibMemo(n-2, memo)));
+
+        }
+
+        return memo.get(n);
+
+    }
+}
+
+ 
