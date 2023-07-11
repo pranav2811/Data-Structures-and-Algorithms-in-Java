c9dadb6f9ee454ee028cb4cc914cafc925cca73f
Pranav Powar <powar.pranav29@gmail.com>
2023-07-11 17:33:36 +0530

    pattern7

--- Patterns/Pattern7.java
+++ Patterns/Pattern7.java
@@ -1,7 +1,28 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class Pattern7 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i = 0; i < n; i++){
+
+        //to print out the spaces before the stars
+        for(int j = n; j >i; j--){
+            System.out.print(" ");
+        }
+        //to print out the stars
+        for(int j = 0; j < 2*i+1; j++){
+            System.out.print("*");
+        }
+        //to print out the spaces after the stars
+        for(int j = n; j > i; j--){
+            System.out.print(" ");
+        }
+        System.out.println();
     }
+    sc.close();
+    }
+    
 }
