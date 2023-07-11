7a662263ddf43e102ae1a4a41be1afba9abbc15a
Pranav Powar <powar.pranav29@gmail.com>
2023-07-11 20:14:50 +0530

    binary number apttern

---
+++ Patterns/Pattern11.java
@@ -0,0 +1,31 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if(i % 2 == 0){
+                    if(j % 2 == 0){
+                        System.out.print("1");
+                    }
+                    else{
+                        System.out.print("0");
+                    }
+                }
+                else{
+                    if(j % 2 == 0){
+                        System.out.print("0");
+                    }
+                    else{
+                        System.out.print("1");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
